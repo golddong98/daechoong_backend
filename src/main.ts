39e8c585c3cc5a6bc973152d936dde2f40e620c4
golddong98 <73287240+golddong98@users.noreply.github.com>
2023-02-19 21:44:17 +0900

    [add] cors 처리

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('NODE_SERVER_PORT');
+  app.enableCors({
+    origin: true,
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
   await app.listen(port);
 }
 
