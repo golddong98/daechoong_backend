b247e7f4e399504f6d590b065a5bf6d2ad485afe
golddong98 <73287240+golddong98@users.noreply.github.com>
2023-03-30 17:41:10 +0900

    [update] get 요청시 쓸데없는 데이터 제외 수정

--- src/resource/notes/notes.service.ts
+++ src/resource/notes/notes.service.ts
@@ -36,15 +36,32 @@ export class NotesService {
 
   async getAllNotes({ userId }) {
     try {
-      const confirmedNotes = await this.notesRepository.find({
-        select: ['id', 'content', 'files'],
-        relations: ['files'],
-        where: { user: userId },
-        order: {
-          updatedAt: 'DESC',
-        },
-      });
-      return confirmedNotes;
+      return await this.notesRepository
+        .createQueryBuilder('note')
+        .select([
+          'note.id',
+          'note.content',
+          'file.id',
+          'file.fileUrl',
+          'file.originalName',
+          'file.mimeType',
+          'file.encoding',
+          'file.size',
+        ])
+        .leftJoin('note.files', 'file')
+        .where(`note.userId = ${userId}`)
+        .orderBy('note.updatedAt', 'DESC')
+        .getMany();
+
+      // await this.notesRepository.find({
+      //   select: ['id', 'content', 'files'],
+      //   relations: ['files'],
+      //   where: { user: userId },
+      //   order: {
+      //     updatedAt: 'DESC',
+      //   },
+      // });
+      // return confirmedNotes;
     } catch (error) {
       throw new BadRequestException('노트를 불러오는데 문제가 생겼습니다.');
     }
